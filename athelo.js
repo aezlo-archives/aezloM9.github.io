48942e2ff1df27a029aded7514f8b3820d91e091
aezloman <aezlo.github@gmail.com>
2018-09-23 10:32:05 +0100

    Create athelo.js

---
+++ athelo.js
@@ -0,0 +1,9 @@
+// My scripts //
+
+var person = prompt("Please enter your name", "name")
+ if (person == null || person == "") {
+  txt = "User exits the prompt.";
+ }
+ else {
+  txt = "Hello " + person + "Welcome to the Aezlo Site";
+ }
